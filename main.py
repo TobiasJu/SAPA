03333c9fa587dbc8e645360730f9cce42b4789c0
Tobias Juhre <Tobias Juhre>
2016-10-14 23:26:06 +0200

    added filter to main script

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import os
 
 
 class Mutation:
-    # __id = ""
+    __id = ""
     __chr = ""
-    __pos = 0
+    __pos = 0   # int
     __ref = ""
     __alt = ""  # mutated Base(es)
     __type = ""
     __context = ""
     __consequence = ""
     __dbSNP = ""
-    __cosmix = ""
+    __cosmic = ""
     __clinVar = ""
-    __qual = 0.0
-    __altFreq = 0.0
-    __totalDepth = 0
-    __refDepth = 0
-    __altDepth = 0
-    __strandBias = "" # 0.0
-
+    __qual = 0  # int
+    __altFreq = 0.0 # float
+    __totalDepth = 0 # int
+    __refDepth = 0  # int
+    __altDepth = 0  # int
+    __strandBias = ""  # 0.0
+    __conclusion = ""
 
     # constructor
-    def __init__(self, chr, pos, ref, alt, type, context, consequence, dbSNP, cosmix, clinVar, qual, altFreq,
-                 totalDepth, refDepth, altDepth, strandBias):
-        # self.__id = id
+    def __init__(self, id, chr, pos, ref, alt, type, context, consequence, dbSNP, cosmic, clinVar, qual, altFreq,
+                 totalDepth, refDepth, altDepth, strandBias, conclusion):
+        self.__id = id
         self.__chr = chr
         self.__pos = pos
         self.__ref = ref
@@ -34,7 +34,7 @@ class Mutation:
         self.__context = context
         self.__consequence = consequence
         self.__dbSNP = dbSNP
-        self.__cosmix = cosmix
+        self.__cosmic = cosmic
         self.__clinVar = clinVar
         self.__qual = qual
         self.__altFreq = altFreq
@@ -42,6 +42,7 @@ class Mutation:
         self.__refDepth = refDepth
         self.__altDepth = altDepth
         self.__strandBias = strandBias
+        self.__conclusion = conclusion
 
     # setter
     def set_id(self, id):
@@ -71,8 +72,8 @@ class Mutation:
     def set_dbSNP(self, dbSNP):
         self.__dbSNP = dbSNP
 
-    def set_cosmix(self, cosmix):
-        self.__cosmix = cosmix
+    def set_cosmic(self, cosmic):
+        self.__cosmic = cosmic
 
     def set_clinVar(self, clinVar):
         self.__clinVar = clinVar
@@ -94,7 +95,6 @@ class Mutation:
 
     def set_strandBias(self, strandBias):
         self.__strandBias = strandBias
-
 
     # getter
     def get_id(self):
@@ -124,8 +124,8 @@ class Mutation:
     def get_dbSNP(self):
         return self.__dbSNP
 
-    def get_cosmix(self):
-        return self.__cosmix
+    def get_cosmic(self):
+        return self.__cosmic
 
     def get_clinVar(self):
         return self.__clinVar
@@ -151,32 +151,77 @@ class Mutation:
     # class functions
 
     def toString(self):
-        return "{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}".format(self.__chr,
-                                                                        self.__pos,
-                                                                        self.__ref,
-                                                                        self.__alt,
-                                                                        self.__type,
-                                                                        self.__context,
-                                                                        self.__consequence,
-                                                                        self.__dbSNP,
-                                                                        self.__cosmix,
-                                                                        self.__clinVar,
-                                                                        self.__qual,
-                                                                        self.__altFreq,
-                                                                        self.__totalDepth,
-                                                                        self.__refDepth,
-                                                                        self.__altDepth,
-                                                                        self.__strandBias)
+        return "ID: {}, Chr: {}, Pos: {}, Ref: {}, Alt: {}, Type: {}, Context: {}, Consequence: {}, dbSNP: {}, " \
+               "COSMIC: {}, ClinVar: {}, Qual: {}, Alt Freq: {}, Total Depth: {},Ref Depth: {}, Alt Depth: {}, " \
+               "Strand Bias: {}, Conclusion: {} ".format(self.__id,
+                                         self.__chr,
+                                         self.__pos,
+                                         self.__ref,
+                                         self.__alt,
+                                         self.__type,
+                                         self.__context,
+                                         self.__consequence,
+                                         self.__dbSNP,
+                                         self.__cosmic,
+                                         self.__clinVar,
+                                         self.__qual,
+                                         self.__altFreq,
+                                         self.__totalDepth,
+                                         self.__refDepth,
+                                         self.__altDepth,
+                                         self.__strandBias,
+                                         self.__conclusion)
 
-    
 
 f = open('data/truseq-amplicon-variants_tobi.csv', 'r')
+line_count = 0
+
+mutations = []
 for line in f:
-    print line
+    # remove /n form end of line
+    lien = line.rstrip('/n')
+    split_line = line.split('","')
+    split_line[0] = split_line[0].translate(None, '"')
+
+    if len(split_line) == 16:
+        # wieso geht das nicht?
+        # mutation1 = Mutation(split_line)
+        line_count += 1
+        mutations.append(Mutation(line_count, split_line[0], split_line[1], split_line[2], split_line[3], split_line[4],
+                                  split_line[5], split_line[6], split_line[7], split_line[8], split_line[9],
+                                  int(split_line[10]), split_line[11], split_line[12], split_line[13], split_line[14],
+                                  split_line[15], "unknown"))
+
 print "fin\n"
 f.close()
 
-mutation1 = Mutation("chr1", 36932474, "C", "T", "SNV", "Coding,Intergenic", "synonymous_variant,upstream_gene_variant",
-                     "rs146209034", "", "", 100, 0.533, 5346, 2489, 2848, "-1.000")
+print(mutations[80].toString())
+print ("Mutation count: ", line_count)
 
-print(mutation1.toString())
+# filter all entries
+i = 0
+for mutation in mutations:
+    # only clinically relevant quality
+    if mutation.get_qual() <= 95:
+        del mutations[i]
+
+    # if mutation does not change the amino acid, it does not affect the cell (in nearly all cases)
+    if mutation.get_consequences() == "synonymous_variant":
+        del mutations[i]
+    # more filters to come
+    i += 1
+print("past filter: ", len(mutations))
+
+for mutation in mutations:
+    if mutation.get_clinVar() != "":
+        print mutation.get_clinVar()
+
+    if mutation.get_dbSNP() != "":
+        print mutation.get_dbSNP()
+
+    if mutation.get_cosmic() != "":
+        print mutation.get_cosmic()
+
+
+# ensebmle API
+
