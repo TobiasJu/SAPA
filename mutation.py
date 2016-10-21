707e1195ecac284dc4bb3f2e5aa0d3dd1979747d
Tobias J <tobias.juhre@bio.uni-giessen.de>
2016-10-21 13:56:20 +0200

    outsourced the Mutation class

---
+++ mutation.py
@@ -0,0 +1,188 @@
+
+
+class Mutation:
+    __id = ""
+    __chr = ""
+    __pos = 0  # int
+    __ref = ""
+    __alt = ""  # mutated Base(es)
+    __type = ""
+    __context = ""
+    __consequence = ""
+    __dbSNP = ""
+    __cosmic = ""
+    __clinVar = ""
+    __qual = 0  # int
+    __altFreq = 0.0  # float
+    __totalDepth = 0  # int
+    __refDepth = 0  # int
+    __altDepth = 0  # int
+    __strandBias = 0.0  # float
+    __conclusion = ""
+
+    # constructor
+    def __init__(self, id, chr, pos, ref, alt, type, context, consequence, dbSNP, cosmic, clinVar, qual, altFreq,
+                 totalDepth, refDepth, altDepth, strandBias, conclusion):
+        self.__id = id
+        self.__chr = chr
+        self.__pos = pos
+        self.__ref = ref
+        self.__alt = alt
+        self.__type = type
+        self.__context = context
+        self.__consequence = consequence
+        self.__dbSNP = dbSNP
+        self.__cosmic = cosmic
+        self.__clinVar = clinVar
+        self.__qual = qual
+        self.__altFreq = altFreq
+        self.__totalDepth = totalDepth
+        self.__refDepth = refDepth
+        self.__altDepth = altDepth
+        self.__strandBias = strandBias
+        self.__conclusion = conclusion
+
+    # setter
+    def set_id(self, id):
+        self.__id = id
+
+    def set_chr(self, chr):
+        self.__chr = chr
+
+    def set_pos(self, pos):
+        self.__pos = pos
+
+    def set_ref(self, ref):
+        self.__ref = ref
+
+    def set_alt(self, alt):
+        self.__alt = alt
+
+    def set_type(self, type):
+        self.__type = type
+
+    def set_context(self, context):
+        if len(context.split(',')) == 1:
+            self.__context = context
+        else:
+            multiple_context = context.split(',')
+            self.__context = multiple_context
+
+    def set_consequence(self, consequence):
+        if len(consequence.split(',')) == 1:
+            self.__context = consequence
+        else:
+            multiple_context = consequence.split(',')
+            self.__context = multiple_context
+
+    def set_dbSNP(self, dbSNP):
+        if len(dbSNP.split(',')) == 1:
+            self.__context = dbSNP
+        else:
+            multiple_context = dbSNP.split(',')
+            self.__context = multiple_context
+
+    def set_cosmic(self, cosmic):
+        if len(cosmic.split(',')) == 1:
+            self.__context = cosmic
+        else:
+            multiple_context = cosmic.split(',')
+            self.__context = multiple_context
+
+    def set_clinVar(self, clinVar):
+        self.__clinVar = clinVar
+
+    def set_qual(selfs, qual):
+        selfs.__qual = qual
+
+    def set_altFreq(self, altFreq):
+        self.__altFreq = altFreq
+
+    def set_totalDepth(self, totalDepth):
+        self.__totalDepth = totalDepth
+
+    def set_refDepth(self, refDepth):
+        self.__refDepth = refDepth
+
+    def set_altDepth(self, altDepth):
+        self.__altDepth = altDepth
+
+    def set_strandBias(self, strandBias):
+        self.__strandBias = strandBias
+
+    # getter
+    def get_id(self):
+        return self.__id
+
+    def get_chr(self):
+        return self.__chr
+
+    def get_pos(self):
+        return self.__pos
+
+    def get_ref(self):
+        return self.__ref
+
+    def get_alt(self):
+        return self.__alt
+
+    def get_type(self):
+        return self.__type
+
+    def get_context(self):
+        return self.__context
+
+    def get_consequences(self):
+        return self.__consequence
+
+    def get_dbSNP(self):
+        return self.__dbSNP
+
+    def get_cosmic(self):
+        return self.__cosmic
+
+    def get_clinVar(self):
+        return self.__clinVar
+
+    def get_qual(self):
+        return self.__qual
+
+    def get_altFreq(self):
+        return self.__altFreq
+
+    def get_totalDepth(self):
+        return self.__totalDepth
+
+    def get_refDepth(self):
+        return self.__refDepth
+
+    def get_altDepth(self):
+        return self.__altDepth
+
+    def get_strandBias(self):
+        return self.__strandBias
+
+    # class functions
+
+    @property
+    def toString(self):
+        return "ID: {}, Chr: {}, Pos: {}, Ref: {}, Alt: {}, Type: {}, Context: {}, Consequence: {}, dbSNP: {}, " \
+               "COSMIC: {}, ClinVar: {}, Qual: {}, Alt Freq: {}, Total Depth: {},Ref Depth: {}, Alt Depth: {}, " \
+               "Strand Bias: {}, Conclusion: {} ".format(self.__id,
+                                                         self.__chr,
+                                                         self.__pos,
+                                                         self.__ref,
+                                                         self.__alt,
+                                                         self.__type,
+                                                         self.__context,
+                                                         self.__consequence,
+                                                         self.__dbSNP,
+                                                         self.__cosmic,
+                                                         self.__clinVar,
+                                                         self.__qual,
+                                                         self.__altFreq,
+                                                         self.__totalDepth,
+                                                         self.__refDepth,
+                                                         self.__altDepth,
+                                                         self.__strandBias,
+                                                         self.__conclusion)
