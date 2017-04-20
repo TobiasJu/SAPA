4da852fd201a1bd3618298d94731d258b7a0c8ad
Twinstar <twinstar2@freenet.de>
2017-04-20 19:07:39 +0200

    added auto comlete input check

--- input_check.py
+++ input_check.py
@@ -6,5 +6,27 @@ def check_input(input_string):
         return "Deleterious"
     elif input_string == "T":
         return "Tolerated"
+    elif input_string == "P":
+        return "Possibly damaging"
+    elif input_string == "B":
+        return "Benign"
+    elif input_string == "D":
+        return "damaging"
+    elif input_string == "N":
+        return "Neutral"
+    elif input_string == "U":
+        return "Unknown"
+    elif input_string == "A":
+        return "disease_causing_automatic"
+    elif input_string == "N":
+        return "polymorphism"
+    elif input_string == "P":
+        return "polymorphism_automatic"
+    elif input_string == "H":
+        return "High"
+    elif input_string == "M":
+        return "Medium"
+    elif input_string == "L":
+        return "Low"
     else:
         return input_string
